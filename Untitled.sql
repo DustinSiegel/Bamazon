9389b6576c6d061a24e584851700aca63490260d
Dustin J Siegel <weenzombie@gmail.com>
2017-08-04 00:26:04 -0600

    displays the database

--- Untitled.sql
+++ Untitled.sql
@@ -6,7 +6,7 @@ USE bamazonDB;
 
 CREATE TABLE products (
 	item_id INT(10) NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(30),
+    product_name VARCHAR(40),
     department_name VARCHAR(30),
     price DECIMAL (9, 2) NOT NULL,
     stock_quantity INT(10),
@@ -14,6 +14,3 @@ CREATE TABLE products (
 );
 
 SELECT * FROM products;
-    
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Big Trouble In Little China", "Movies", 9.99, 31);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,5 +11,28 @@ var connection = mysql.createConnection({
 });
 connection.connect(function(err) {
   if (err) throw err;
-  runSearch();
+  showDataBase();
 });
+
+function showDataBase() { //Distplays the items for sale.
+	connection.query("SELECT * FROM products", function(err, data) {
+    	if (err) throw err;
+    	var itemArray = [];
+        for (var i = 0; i < data.length; i++) {
+            itemArray.push(data[i].item_id, data[i].product_name, data[i].department_name, data[i].price, data[i].stock_quantity);
+        }
+      	console.log(itemArray);
+    });
+};
+
+function userPrompt() { //asks the customer for an id number for the desired item and a quantity to purchase.
+
+};
+
+function checkQuantity() {	//Checkes if there are enough of the item requested in stock to fulfill the customer order. Alerts the user if there is insufficient quantities.
+
+};
+
+function fulfillOrder() { //Updates the database to reflect the removal of the customer order and gives a total cost for the order.
+
+};
