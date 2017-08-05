9ecb2d9de26a01d5c39701260f5efec4f1e48f13
Dustin J Siegel <weenzombie@gmail.com>
2017-08-04 20:52:47 -0600

    Cleaned up the logs and started the prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,24 +15,68 @@ connection.connect(function(err) {
 });
 
 function showDataBase() { //Distplays the items for sale.
-	connection.query("SELECT * FROM products", function(err, data) {
-    	if (err) throw err;
-    	var itemArray = [];
+	var query = "SELECT item_id,product_name,department_name,price FROM products";
+	connection.query(query, function(err, data) {
+    	    
         for (var i = 0; i < data.length; i++) {
-            itemArray.push(data[i].item_id, data[i].product_name, data[i].department_name, data[i].price, data[i].stock_quantity);
-        }
-      	console.log(itemArray);
-    });
+            
+            console.log("---------------------#" + data[i].item_id + "---------------------");
+			console.log("Product Name: " + data[i].product_name + "\n" + "Catagory: " + data[i].department_name + "\n" + "Price: $" + data[i].price);
+		};
+		
+		if (err) throw err;
+
+		else {
+			userPrompt();
+		};
+      	
+    });  
 };
 
 function userPrompt() { //asks the customer for an id number for the desired item and a quantity to purchase.
-
+	inquirer
+	    .prompt([
+	      	{
+	        	name: "itemChoice",
+	        	type: "input",
+	        	message: "Enter the number of product you would like to perchase. ",
+	        	validate: function(value) {
+	         	
+	         	if (isNaN(value) === false) {
+	            	return true;
+	         	}
+	          	
+	          	return false;
+	        	}
+	      	},
+	      	{
+	        	name: "itemQuantity",
+	        	type: "input",
+	        	message: "How many of these would you like to purchase? ",
+	        	validate: function(value) {
+	          	
+	          		if (isNaN(value) === false) {
+	            		return true;
+	          		}
+	        	
+	        		return false;
+	      		  	}
+	      	}
+	    ])
+	    .then(function(answer) {
+	      	var query = "SELECT item_id,product_name,department_name,price FROM products";
+	      	connection.query(query, [answer.itemChoice, answer.itemQuantity], function(err, res) {
+	        	for (var i = 0; i < res.length; i++) {
+	        		console.log("You have selected: ");
+	          		console.log("Product Name: " + data[i].product_name + "\n" + "Catagory: " + data[i].department_name + "\n" + "Price: $" + data[i].price);
+				};
+			});
+	    });
 };
+// function checkQuantity() {	//Checkes if there are enough of the item requested in stock to fulfill the customer order. Alerts the user if there is insufficient quantities.
 
-function checkQuantity() {	//Checkes if there are enough of the item requested in stock to fulfill the customer order. Alerts the user if there is insufficient quantities.
+// };
 
-};
+// function fulfillOrder() { //Updates the database to reflect the removal of the customer order and gives a total cost for the order.
 
-function fulfillOrder() { //Updates the database to reflect the removal of the customer order and gives a total cost for the order.
-
-};
+// };
